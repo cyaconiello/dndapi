82dcf1867421e7711255411b2721e0ae12b9810b
Christopher Yaconiello <chris@yaconiello.com>
2022-03-08 18:10:34 -0500

    removing commented out data manipulation on the view

--- backend/dndapi/characters/views.py
+++ backend/dndapi/characters/views.py
@@ -10,37 +10,7 @@ class CharactersListCreateView(generics.ListCreateAPIView):
     queryset = Character.objects.all()
     serializer_class = CharacterSerializer
 
-    # def perform_create (self, serializer: CharacterSerializer):
-    #     race = fetch_race_object(self.request)
-    #     if race:
-    #         serializer.save(race=race.first())
-    #     return super().perform_create(serializer)
-
-
 class CharacterDeatailsRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Character.objects.all()
     serializer_class = CharacterSerializer
     lookup_field = "character_uuid"
-
-    # def update(self, request, *args, **kwargs):
-    #     instance = self.get_object()
-    #     race = fetch_race_object(request)
-    #     if race:
-    #         instance.race = race.first()
-    #         instance.save()
-    #     return super().update(request, *args, **kwargs)
-
-# put in utility file?
-# def fetch_race_object(request):
-#     race = {}
-#     if "race_attributes" in request.data:
-#         if "uuid" in request.data["race_attributes"]:
-#             race = Race.objects.filter(
-#                 race_uuid=request.data["race_attributes"]["uuid"]
-#             )
-#         if not race and "name" in request.data["race_attributes"]:
-#             race = Race.objects.filter(
-#                 name__iexact=request.data["race_attributes"]["name"]
-#             )
-#             pass
-#     return race
